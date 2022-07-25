7b761d16abe363e9b1762818aa03ca5de97db004
Kateřina Pelegrinová <pelegrinovak@gmail.com>
2022-07-25 17:37:19 +0200

    Přidání obrázků a dat
    Přidání obrazáků pro vykreslení hry a dat pro herní otázky a odpovědi.

---
+++ data.py
@@ -0,0 +1,36 @@
+data = [{"slovo": "program", "jazyk": "řeckého", "etymologie": "přejato přes něm. Programm a lat. programma; z řec. prógramma = 'vyhláška', doslovně 'co je předem napsáno' (od prográfō = 'předem píšu')"},
+        {"slovo": "myš", "jazyk": "praindoevropského", "etymologie": "z praindoev. tvaru *mūs- od *meus- = 'rychle se pohybovat, krást'"} ,
+        {"slovo": "myslet", "jazyk": "praindoevropského", "etymologie": "přejato přes praindoev. *mūdh-sl- a následné prasl. *myslƄ; z praindoev.  *meudh- = 'myslet, dbát'"},
+        {"slovo": "tuleň", "jazyk": "laponského", "etymologie": "Preslem převzato z rus. tjulén, kde pravděpodobně výpůjčka z laponštiny (tuĺĺa/dullja)"},
+        {"slovo": "žížala", "jazyk": "praslovanského", "etymologie": "přes staročeské žúžala, žúžela, z psl. *žuža označující různé druhy hmyzu, zřejmě od onomatopoického slovesa *žužati napodobujícího hmyzí bzučení (srov. rus. žužžát = 'bzučet', žuk = 'brouk')"},
+        {"slovo": "rum (alkoholický nápoj)", "jazyk": "anglického", "etymologie": "přejato přes něm. Rum; z angl. rum – v řeči anglických osadníků Barbadosu v 17. stol. zkrácenina nářečního rumbullion = 'pozdvižení, povyk', které je onomatopoického původu"},
+        {"slovo": "sloh", "jazyk": "ruského", "etymologie": "převzato v obrození z rus. slog"},
+        {"slovo": "šalotka", "jazyk": "latinského", "etymologie": "šalotka: přejato přes něm. Schalotte, zde přes fran. échalote – z lat. (cēpa) Ascalōnia = doslova 'askalonská (cibule)': podle palestinského města Askalon"},
+        {"slovo": "šál", "jazyk": "perského", "etymologie": "přejato přes něm. Schal, fran. châle, angl. shawl; z per. šāl = odvozuje se od místního indického jména Šāliāt"},
+        {"slovo": "otoman", "jazyk": "arabského", "etymologie": "z fran. ottomane = 'osmanská (turecká) pohovka' od arab. Othmān (v tur. výslovnosti Osman), jména zakladatele osmanské říše ze 14. stol."},
+        {"slovo": "papuč", "jazyk": "perského", "etymologie": "asi přejato přes maď. papucs z tur. papuč = 'střevíc', to z per. pāpūš = 'pokrývka nohou' (z pā = 'noha' + pūšīdän = 'pokrývat')"},
+        {"slovo": "etymologie", "jazyk": "řeckého", "etymologie": "přejato přes lat. etymologia; z řec. etymología od étymos = 'správný, pravdivý' (zavedeno starořeckými filosofy, kteří zkoumali, zda slova „pravdivě“ odpovídají označovaným věcem) + -logie (vysvětleno v samostatné etym.)"},
+        {"slovo": "-logie", "jazyk": "řeckého", "etymologie": "= 'věda'; z řec. -logía – ve složeninách znamená dějové jméno k légō = 'sbírám, čtu, mluvím' (-log = 'vědec' z řec. -lógos – ve složeninách činitelské jméno k légō)"},
+        {"slovo": "lišaj", "jazyk": "praslovanského", "etymologie": "převzato Jungmannem ze sloven.; z praslov. *lichъ = 'špatný, zlý' (noční motýli, zvláště lišaj smrtihlav, působili jako zlověstná znamení)"},
+        {"slovo": "šňůra", "jazyk": "praindoevropského", "etymologie": "přejato ze středohornoněmeckého snuor (dnes Schnur); z praindoev. *(s)ner- = 'kroutit, vinout'"},
+        {"slovo": "žádat", "jazyk": "praindoevropského", "etymologie": "z praslov. *žȩdati; z praindoev. *gꭟhedh- = 'žádat, toužit'"},
+        {"slovo": "žaluzie", "jazyk": "řeckého", "etymologie": "přejato přes něm. Jalousie – zde z fran. jalousie a it. gelosia = původně 'žárlivost, závist', z it. termínu trubadúrské lyriky gelos = 'horlivý, žárlivý', z lat. zēlōsus; z pův. řec. zēlos = 'horlivost, žárlivost, závist' (v Itálii a Francii takto v 16. stol. nazvána neprůhledná dílcová zástěna v okně, která chránila před žárlivými či závistivými pohledy zvenčí)"},
+        {"slovo": "skříň", "jazyk": "latinského", "etymologie": "přejato přes starohornoněmecké scrīni (něm. Schrein); z lat. scrīnium = 'pouzdro, krabice, skříň'"},
+        {"slovo": "cukr", "jazyk": "indického", "etymologie": "přejato přes středohornoněmeckého zucker z it. zuchcero – zde z arab. sukkar (z arab. je také fran. sucre, odsud angl. sugar X rus. sáchar přejato přes řec. sákcharon); původně z ind. sakkharā"},
+        {"slovo": "lemur", "jazyk": "latinského", "etymologie": "z lat. lemurēs = 'duchové zemřelých' – podle jejich bizarního vzhledu a nočního způsobu života"},
+        {"slovo": "vir(us)", "jazyk": "latinského", "etymologie": "převzato přes moderní evropské jazyky (něm. Virus, fran., angl. virus), v nichž bylo v 19. stol. pro potřeby soudobé medicíny přejato z pův. lat. vīrus = 'jed, otravná šťáva, sliz'"},
+        {"slovo": "vezír", "jazyk": "arabského", "etymologie": "přejato přes tur. vezīr; z pův. arab. wazīr = doslova 'kdo nese břemeno' od wazara = 'nést břemeno'"},
+        {"slovo": "fara", "jazyk": "řeckého", "etymologie": "přejato přes středohornoněmecké pharre, zde z lat. parrochia; z pův. řec. pároikos = 'soused, cizinec', doslova 'vedle bydlící' (z para = 'vedle' + oĩkos = 'dům, obydlí'"},
+        {"slovo": "špeluňka", "jazyk": "řeckého", "etymologie": "přejato přes něm. Spelunke, zde z lat. spēlunca = 'jeskyně, doupě'; z pův. řec. spēlynx = 'jesykně' (odsud také speleologie)"},
+        {"slovo": "špagety", "jazyk": "italského", "etymologie": "přejato přes něm. Spaghetti; původně z it. spaghetti = doslova 'provázky', od spago = 'provaz'"},
+        {"slovo": "batik", "jazyk": "malajského", "etymologie": "přejato přes niz. batik; původně z malajského batik = 'kropenatý'"},
+        {"slovo": "bažant", "jazyk": "řeckého", "etymologie": "přejato přes středohornoněmecké fasant, zde z lat. fasianus; původně z řec. fasianós, a to podle jména řeky Fasis na jižním úpatí Kavkazu, kde je domov bažantů"},
+        {"slovo": "nafta", "jazyk": "perského", "etymologie": "přejato přes něm. Naphta, z lat. naphtha, z řec. náftha; původně z per. naft = 'zemní olej'"},
+        {"slovo": "pergamen", "jazyk": "latinského", "etymologie": "z lat. (charta) pergamēna = doslova 'pergamenský (papír)', podle maloasijského města Pergamon, kde ve 2. stol. př. n. l. vyvinuta výroba pergamenu jako náhražky papyru"},
+        {"slovo": "lama", "jazyk": "kečuánského", "etymologie": "přejato přes špaň. llama; původně z kečuánského llama (kečuánština = jazyk peruánských Indiánů)"}
+        ]
+
+#{"slovo": "", "jazyk": "", "etymologie": ""}
+#mozne_odpovedi = ["anglického", "praslovanského", "ruského", "řeckého", "praindoevropského", "latinského", "arabského", "perského", "indického", "italského", "malajského", "kečuánského", "laponského"]   
+
+data_napl = tuple(data)
